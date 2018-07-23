8225e26adf9d13da5a23701873ae775ec3cb129a
mthdht <mthdht@gmail.com>
2018-07-23 21:04:48 +0200

    change jumbotron layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,15 +2,17 @@
 
 @section('jumbotron')
     <div class="jumbotron ct-camel-light">
-        <div class="w3-content w3-padding-16 w3-padding w3-center">
-            <div class="w3-col m5 l4">
-                <img src="/img/moi.jpg" alt="photo de matthieu dehondt" class="w3-circle w3-image">
-                <h1>Matthieu Dehondt</h1>
-                <p>Développeur | Formateur</p>
+        <div class="w3-row w3-center">
+            <div class="w3-col m6 l6">
+                <img src="/img/moi.jpg" alt="photo de matthieu dehondt" class="w3-circle w3-image w3-padding-large" width="300px">
+                <div class="w3-animate-left">
+                    <h1>Matthieu Dehondt</h1>
+                    <p class="w3-large">Développeur | Formateur</p>
+                </div>
             </div>
 
-            <div class="w3-col m7 l8 w3-hide-small">
-                <h2>Bienvenue sur mon site !</h2>
+            <div class="w3-col m6 l6 w3-hide-small w3-left-align w3-padding-32 w3-animate-right">
+                <h2 class="w3-small"><b>BIENVENUE SUR MON SITE !</b></h2>
             </div>
         </div>
     </div>
@@ -50,9 +52,9 @@
     </div>
 
     <section class="skills w3-row w3-padding w3-padding-48 w3-center">
-        <h2>Mes compétences</h2>
+        <h2 class="w3-xxlarge">Compétences</h2>
         <div class="w3-row">
-            <div class="w3-col l6">
+            <div class="w3-col l6 w3-padding-24">
                 <h3>Langages</h3>
                 <div class="skillbar w3-dark-gray w3-margin-top">
                     <div class="html w3-padding-large w3-red" style="width: 90%">
@@ -121,35 +123,35 @@
                 </div>
             </div>
 
-            <div class="w3-col l6 w3-padding-24">
+            <div class="w3-col l6 w3-padding-24 w3-row-padding">
                 <h3>Outils</h3>
 
-                <div class="tool w3-col s6 m4 w3-center">
+                <div class="tool w3-col s4 w3-center">
                     <img src="/img/git.png" alt="logo de git" class="w3-image">
                     <p>Git</p>
                 </div>
 
-                <div class="tool w3-col s6 m4 w3-center">
+                <div class="tool w3-col s4 w3-center">
                     <img src="/img/github.png" alt="logo github" class="w3-image">
                     <p>Github</p>
                 </div>
 
-                <div class="tool w3-col s6 m4 w3-center">
+                <div class="tool w3-col s4 w3-center">
                     <img src="/img/react.png" alt="logo react" class="w3-image">
                     <p>React</p>
                 </div>
 
-                <div class="tool w3-col s6 m4 w3-center">
+                <div class="tool w3-col s4 w3-center">
                     <img src="/img/vue.png" alt="logo vuejs" class="w3-image">
                     <p>Vue</p>
                 </div>
 
-                <div class="tool w3-col s6 m4 w3-center">
+                <div class="tool w3-col s4 w3-center">
                     <img src="/img/laravel.png" alt="logo laravel" class="w3-image">
                     <p>Laravel</p>
                 </div>
 
-                <div class="tool w3-col s6 m4 w3-center">
+                <div class="tool w3-col s4 w3-center">
                     <img src="/img/phpstorm.png" alt="logo phpstorm" class="w3-image">
                     <p>PhpStorm</p>
                 </div>
@@ -159,8 +161,17 @@
     </section>
 
     <section class="w3-padding w3-padding-48 w3-center ct-red">
-        <h2>Portfolio</h2>
-        <p class="w3-large">Mes projets récents</p>
+        <h2 class="w3-xxlarge">Projets</h2>
+        <p class="w3-large">Mes réalisations récentes</p>
+
+        <div class="projects w3-row">
+            <div class="project w3-col m6 l4"></div>
+            <div class="project w3-col m6 l4"></div>
+            <div class="project w3-col m6 l4"></div>
+            <div class="project w3-col m6 l4"></div>
+            <div class="project w3-col m6 l4"></div>
+            <div class="project w3-col m6 l4"></div>
+        </div>
     </section>
 @endsection
 
