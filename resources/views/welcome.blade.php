a4efa2e4fe87b550a3d64d950519edd1d6693e2c
mthdht <mthdht@gmail.com>
2018-07-23 10:11:21 +0200

    start skills section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,35 +18,126 @@
 
 @section('content')
     <div class="services w3-row">
-        <div class="w3-col m6 l4 ct-dark-blue w3-hover-opacity">
+        <div class="w3-col m4 l4 ct-dark-blue w3-hover-opacity">
             <a href="projets">
                 <div class="w3-center w3-padding-large w3-padding-32">
                     <i class="fas fa-archive fa-4x"></i>
                     <h3>Mes projets</h3>
-                    <p>Découvrir mes différents projets personnel et mes travaux en cour!</p>
+                    <p>Découvrir mes différents projets personnel, mes réalisations et autres travaux en cour!</p>
                 </div>
             </a>
         </div>
 
-        <div class="w3-col m6 l4 ct-blue w3-hover-opacity">
+        <div class="w3-col m4 l4 ct-blue w3-hover-opacity">
             <a href="">
                 <div class="w3-center w3-padding-large w3-padding-32">
                     <i class="fas fa-gamepad fa-4x"></i>
                     <h3>Mes jeu</h3>
-                    <p>Jouer à des reproduction de jeu tel que le Mastermind, Jeu des Paires, Tic Tac Toe ...!</p>
+                    <p>Jouer à des reproduction de jeu tel que le Mastermind, Jeu des Paires, Tic Tac Toe, Shi-Fu-Mi ...!</p>
                 </div>
             </a>
         </div>
 
-        <div class="w3-col m6 l4 ct-red w3-hover-opacity">
+        <div class="w3-col m4 l4 ct-red w3-hover-opacity">
             <a href="">
                 <div class="w3-center w3-padding-large w3-padding-32">
                     <i class="fas fa-book fa-4x"></i>
                     <h3>Mes cours</h3>
-                    <p>Apprendre à le développement web avec différents cours sur HTML, CSS, Javascript et PHP !</p>
+                    <p>Apprendre le développement web avec différents cours sur HTML, CSS, Javascript et PHP !</p>
                 </div>
             </a>
         </div>
     </div>
+
+    <section class="skills w3-row w3-padding w3-padding-48 w3-center">
+        <h2>Mes compétences</h2>
+        <div class="w3-row">
+            <div class="w3-col l6">
+                <div class="skillbar w3-dark-gray w3-margin-top">
+                    <div class="html w3-padding-large w3-red" style="width: 90%">
+                        <div class="skill-title w3-left">
+                            <b>HTML</b>
+                        </div>
+                        <div class="skill-pourcent w3-right">
+                            90%
+                        </div>
+                    </div>
+                </div>
+
+                <div class="skillbar w3-dark-gray w3-margin-top">
+                    <div class="html w3-padding-large w3-indigo" style="width: 75%">
+                        <div class="skill-title w3-left">
+                            <b>CSS</b>
+                        </div>
+                        <div class="skill-pourcent w3-right">
+                            75%
+                        </div>
+                    </div>
+                </div>
+
+                <div class="skillbar w3-dark-gray w3-margin-top">
+                    <div class="html w3-padding-large w3-amber w3-text-white" style="width: 70%">
+                        <div class="skill-title w3-left">
+                            <b>JAVASCRIPT</b>
+                        </div>
+                        <div class="skill-pourcent w3-right">
+                            70%
+                        </div>
+                    </div>
+                </div>
+
+                <div class="skillbar w3-dark-gray w3-margin-top">
+                    <div class="html w3-padding-large w3-blue" style="width: 55%">
+                        <div class="skill-title w3-left">
+                            <b>REACT</b>
+                        </div>
+                        <div class="skill-pourcent w3-right">
+                            55%
+                        </div>
+                    </div>
+                </div>
+
+                <div class="skillbar w3-dark-gray w3-margin-top">
+                    <div class="html w3-padding-large w3-green" style="width: 60%">
+                        <div class="skill-title w3-left">
+                            <b>VUE</b>
+                        </div>
+                        <div class="skill-pourcent w3-right">
+                            60%
+                        </div>
+                    </div>
+                </div>
+
+                <div class="skillbar w3-dark-gray w3-margin-top">
+                    <div class="html w3-padding-large w3-blue-gray" style="width: 80%">
+                        <div class="skill-title w3-left">
+                            <b>PHP</b>
+                        </div>
+                        <div class="skill-pourcent w3-right">
+                            80%
+                        </div>
+                    </div>
+                </div>
+
+                <div class="skillbar w3-dark-gray w3-margin-top">
+                    <div class="html w3-padding-large w3-deep-orange" style="width: 75%">
+                        <div class="skill-title w3-left">
+                            <b>LARAVEL</b>
+                        </div>
+                        <div class="skill-pourcent w3-right">
+                            75%
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            </div>
+        </div>
+    </section>
+
+    <section class="w3-padding w3-padding-48 w3-center">
+        <h2>Portfolio</h2>
+        <p>Mes projets récents</p>
+    </section>
 @endsection
 
