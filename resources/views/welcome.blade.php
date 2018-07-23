a0885bdfeed0a3adadc7b0509c97ef83fef2d607
mthdht <mthdht@gmail.com>
2018-07-23 10:39:16 +0200

    correct style for services section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,32 +18,32 @@
 
 @section('content')
     <div class="services w3-row">
-        <div class="w3-col m4 l4 ct-dark-blue w3-hover-opacity">
+        <div class="w3-col m6 l4 ct-dark-blue w3-hover-opacity service">
             <a href="projets">
                 <div class="w3-center w3-padding-large w3-padding-32">
                     <i class="fas fa-archive fa-4x"></i>
                     <h3>Mes projets</h3>
-                    <p>Découvrir mes différents projets personnel, mes réalisations et autres travaux en cour!</p>
+                    <p class="w3-large">Découvrir mes différents projets personnel, réalisations et travaux en cour !</p>
                 </div>
             </a>
         </div>
 
-        <div class="w3-col m4 l4 ct-blue w3-hover-opacity">
+        <div class="w3-col m6 l4 ct-blue w3-hover-opacity service">
             <a href="">
                 <div class="w3-center w3-padding-large w3-padding-32">
                     <i class="fas fa-gamepad fa-4x"></i>
                     <h3>Mes jeu</h3>
-                    <p>Jouer à des reproduction de jeu tel que le Mastermind, Jeu des Paires, Tic Tac Toe, Shi-Fu-Mi ...!</p>
+                    <p class="w3-large">Jouer à des reproduction de jeu: Mastermind, Jeu des Paires, Morpion, Shi-Fu-Mi ...!</p>
                 </div>
             </a>
         </div>
 
-        <div class="w3-col m4 l4 ct-red w3-hover-opacity">
+        <div class="w3-col l4 ct-red w3-hover-opacity service">
             <a href="">
                 <div class="w3-center w3-padding-large w3-padding-32">
                     <i class="fas fa-book fa-4x"></i>
                     <h3>Mes cours</h3>
-                    <p>Apprendre le développement web avec différents cours sur HTML, CSS, Javascript et PHP !</p>
+                    <p class="w3-large">Apprendre le développement web avec différents cours sur HTML, CSS, Javascript et PHP</p>
                 </div>
             </a>
         </div>
@@ -65,7 +65,7 @@
                 </div>
 
                 <div class="skillbar w3-dark-gray w3-margin-top">
-                    <div class="html w3-padding-large w3-indigo" style="width: 75%">
+                    <div class="html w3-padding-large w3-blue" style="width: 75%">
                         <div class="skill-title w3-left">
                             <b>CSS</b>
                         </div>
@@ -87,23 +87,12 @@
                 </div>
 
                 <div class="skillbar w3-dark-gray w3-margin-top">
-                    <div class="html w3-padding-large w3-blue" style="width: 55%">
+                    <div class="html w3-padding-large w3-green" style="width: 65%">
                         <div class="skill-title w3-left">
-                            <b>REACT</b>
+                            <b>MySQL</b>
                         </div>
                         <div class="skill-pourcent w3-right">
-                            55%
-                        </div>
-                    </div>
-                </div>
-
-                <div class="skillbar w3-dark-gray w3-margin-top">
-                    <div class="html w3-padding-large w3-green" style="width: 60%">
-                        <div class="skill-title w3-left">
-                            <b>VUE</b>
-                        </div>
-                        <div class="skill-pourcent w3-right">
-                            60%
+                            65%
                         </div>
                     </div>
                 </div>
@@ -129,8 +118,6 @@
                         </div>
                     </div>
                 </div>
-            </div>
-
             </div>
         </div>
     </section>
