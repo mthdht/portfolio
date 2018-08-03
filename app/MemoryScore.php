90fa04779f337cd74cc7d47cc731b375b83b78d5
mthdht <mthdht@gmail.com>
2018-08-03 21:46:15 +0200

    made memory score model and add memory score method on user model

--- app/MemoryScore.php
+++ app/MemoryScore.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class MemoryScore extends Model
 {
-    //
+    protected $fillable = ["score", "guesses", "user_id"];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
